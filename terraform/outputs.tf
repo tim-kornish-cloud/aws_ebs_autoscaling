ea4019d802ff52aa1eb1ca92e957175559d0caa0
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-08 15:34:44 -0700

    tinkering with other output, removed at the end

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,8 @@
 # CreatedDate: 9/08/2025
 # Description: set up console output to log the dns name of the load balancer
 
-
+# output dns of load balancer
 output "dns_name" {
   value = aws_lb.tk_lb.dns_name
 }
+
