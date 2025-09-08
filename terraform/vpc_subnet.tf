795a3f5b5d534b3151cab6d3270996a07228e7a9
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-08 13:05:00 -0700

    reconfigure vpc from data to a resource

--- terraform/vpc_subnet.tf
+++ terraform/vpc_subnet.tf
@@ -6,9 +6,12 @@
 # data resources allow you to get information about infrastructure components
 # that already exist. These data objects get the default VPC and subnets that
 # AWS creates for all accounts.
-data "aws_vpc" "default" {
-  default = true
-}
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+resource  "aws_vpc" "default_vpc" {}
+
+data "aws_subnet" "default_subnet" {
+  vpc_id = aws_vpc.default_vpc.id
+  # filter {
+  #   name   = "vpc-id"
+  #   values = [data.aws_default_vpc.default_vpc.id]
+  # }
 }
