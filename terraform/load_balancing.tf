4969e4960bc16c7940d9d5424be942fdeeebb62e
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-08 10:44:53 -0700

    set up load balancer to distribute traffic across nodes

---
+++ terraform/load_balancing.tf
@@ -0,0 +1,55 @@
+# Author: Timothy Kornish
+# CreatedDate: 9/08/2025
+# Description: set up an load balancer to disperse traffic across nodes avoiding bottlenecks
+
+
+resource "aws_lb" "tk_lb" {
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ssh_http_only.id]
+  subnets            = data.aws_subnet_ids.default.ids
+}
+
+resource "aws_lb_target_group" "tk_lb_target_group" {
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 5
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "tk_lb_listener" {
+  load_balancer_arn = aws_lb.tk_lb.id
+  port              = 80
+  protocol          = "HTTP"
+  depends_on        = [aws_lb_target_group.tk_lb_target_group]
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+resource "aws_lb_listener_rule" "tk_lb_listener_rule" {
+  listener_arn = aws_lb_listener.tk_lb_listener.arn
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tk_lb_target_group.arn
+  }
+}
