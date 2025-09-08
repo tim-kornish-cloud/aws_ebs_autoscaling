c2a09d0700b49662bf9d5d5b1bbf1df23a5aa7a6
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-08 10:45:06 -0700

    set up auto scaling to increase number of nodes as needed

---
+++ terraform/auto_scaling.tf
@@ -0,0 +1,16 @@
+# Author: Timothy Kornish
+# CreatedDate: 9/08/2025
+# Description: set up an autoscaling group for the ebs configuration
+
+resource "aws_autoscaling_group" "tk_autoscale_group" {
+  launch_configuration = aws_launch_configuration.tk_lc.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  target_group_arns    = [aws_lb_target_group.tk_lb_target_group.arn]
+  health_check_type    = "ELB"
+  min_size             = var.min_size
+  max_size             = var.max_size
+  desired_capacity     = var.desired_size
+  lifecycle {
+    create_before_destroy = true
+  }
+}
