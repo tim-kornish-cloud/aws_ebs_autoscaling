41f6d2a8d7d3cde2dea710517ebde9d0c7542e54
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-08 10:38:14 -0700

    set up s3 bucket for docker tar file

---
+++ terraform/s3.tf
@@ -0,0 +1,23 @@
+# Author: Timothy Kornish
+# CreatedDate: 9/08/2025
+# Description: set up s3 bucket and folder to store docker tar file
+
+resource "aws_s3_bucket" "tk_s3_bucket" {
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "tk_access_block" {
+  bucket              = aws_s3_bucket.tk_s3_bucket.id
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+resource "aws_s3_bucket_object" "tk_bucket_object" {
+  bucket = aws_s3_bucket.tk_s3_bucket.id
+  key    = var.docker_img_tar_file
+  source = "../${var.docker_img_tar_file}"
+
+  # This gets the md5 checksum of the image file and checks to see if it has
+  # changed since the last apply
+  etag = filemd5("../${var.docker_img_tar_file}")
+}
