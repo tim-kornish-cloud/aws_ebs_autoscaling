e12e75367f6319f6096df1c1d5f857db4c39494b
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-08 10:45:23 -0700

    create dns output to console on terraform apply

---
+++ terraform/outputs.tf
@@ -0,0 +1,8 @@
+# Author: Timothy Kornish
+# CreatedDate: 9/08/2025
+# Description: set up console output to log the dns name of the load balancer
+
+
+output "dns_name" {
+  value = aws_lb.tk_lb.dns_name
+}
